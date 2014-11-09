ee8d622f5b3d5a29012c6cb002ae5f598ef1efe5
Vladimir <ZeleniyD@gmail.com>
2014-11-10 00:06:12 +0200

    add clearfix

--- js/main.js
+++ js/main.js
@@ -23,7 +23,6 @@ angular.module('tictac')
             }
             $scope.player.reverse();
             cell.player=player;
-            console.info(_.where($scope.cells, { 'player': null}).length===0);
            if(_.where($scope.cells, { 'player': null}).length===0){
                if (checkEnd($scope.cells)){
                    winnerName (player);
@@ -58,7 +57,6 @@ angular.module('tictac')
             }
         }
         function checkEnd(t) {
-            console.info(t[0].player=='player' && t[1].player=='player' && t[2].player=='player');
             if (t[0].player=='ai' && t[1].player=='ai' && t[2].player=='ai' || t[0].player=='player' && t[1].player=='player' && t[2].player=='player')  return true;
             if (t[3].player=='ai' && t[4].player=='ai' && t[5].player=='ai' || t[3].player=='player' && t[4].player=='player' && t[5].player=='player')  return true;
             if (t[6].player=='ai' && t[7].player=='ai' && t[8].player=='ai' || t[6].player=='player' && t[7].player=='player' && t[8].player=='player')  return true;
@@ -67,13 +65,5 @@ angular.module('tictac')
             if (t[2].player=='ai' && t[5].player=='ai' && t[8].player=='ai' || t[2].player=='player' && t[5].player=='player' && t[8].player=='player')  return true;
             if (t[0].player=='ai' && t[4].player=='ai' && t[8].player=='ai' || t[0].player=='player' && t[4].player=='player' && t[8].player=='player')  return true;
             if (t[2].player=='ai' && t[4].player=='ai' && t[6].player=='ai' || t[2].player=='player' && t[4].player=='player' && t[6].player=='player')  return true;
-          //  if(t[0].player!==null && t[1].player!==null && t[2].player!==null && t[3].player!==null && t[4].player!==null && t[5].player!==null && t[6].player!==null && t[7].player!==null && t[8].player!==null) return true;
         }
-
-
     })
-    .directive('cell', function() {
-        return {
-            template: 'Name:    {{customer.name}} Address: {{customer.address}}'
-        };
-    });
